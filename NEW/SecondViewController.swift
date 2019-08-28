0efd13b4ace125a66465218f9a4861b31246f60e
Sasha Glasson <sashaglasson@icloud.com>
2019-08-28 14:29:30 +0100

    Initial Commit

---
+++ NEW/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NEW
+//
+//  Created by Sasha Glasson on 28/08/2019.
+//  Copyright © 2019 Sasha Glasson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
